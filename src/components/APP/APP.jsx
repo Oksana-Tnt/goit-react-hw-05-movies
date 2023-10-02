bdf9b4adf911be11d61461a204951dc665c3405a
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-10-02 15:18:20 +0200

    update task5

--- src/components/APP/APP.jsx
+++ src/components/APP/APP.jsx
@@ -1,9 +1,8 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import MovieDetails from 'components/MovieList/MovieDetails';
 import Movies from 'components/Pages/Movies';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from 'components/Layout/Layout';
 import HomePage from 'components/Pages/HomePage';
 import NonFound from 'components/NonFound/NonFound';
@@ -11,14 +10,16 @@ import NonFound from 'components/NonFound/NonFound';
 const APP = () => {
   return (
     <>
-    <Routes>
+      <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />|
-        <Route path="movies" element={<Movies />} />|
-        <Route path=":movieId" element={<MovieDetails />} />|
-        <Route path="*" element={<NonFound />} />|
-        </Route>|
-      </Routes></>
+          <Route index element={<HomePage />} />|
+          <Route path="movies" element={<Movies />} />|
+          <Route path=":movieId" element={<MovieDetails />} />|
+          <Route path="*" element={<NonFound />} />|
+        </Route>
+        |
+      </Routes>
+    </>
   );
 };
 
--- src/components/MovieList/MovieDetails.jsx
+++ src/components/MovieList/MovieDetails.jsx
@@ -1,31 +1,35 @@
-import { getMovieById } from "Services/Film-api";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { getMovieById } from 'Services/Film-api';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const MovieDetails = () => {
-  const {movieId} = useParams();
-  
-  const[movie, setMovie] = useState(null);
+  const { movieId } = useParams();
 
-  useEffect(()=>{
-    const movieById = async (movieId) => {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const movieById = async movieId => {
       try {
         const data = await getMovieById(movieId);
         setMovie(data.data);
-              
+        console.log(data.data);
       } catch (err) {
         console.log(err);
       }
     };
 
     movieById(movieId);
-
   }, [movieId]);
 
-  
-  
-
-  return (<div>Now showing product with id - {movieId}</div>);
+  return (
+    <div>
+      {/* <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} /> */}
+      <h2>{movie.title}</h2>
+      <h3>Overview</h3>
+      <p>{movie.overview}</p>
+      <h3>Genres</h3>
+    </div>
+  );
 };
 
-export default MovieDetails
+export default MovieDetails;
